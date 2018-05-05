280f7dab59f4e4afcaa1264ffeb28f9068ff0d8a
Web Design Zanzibar <support@webdesign-zanzibar.website>
2018-05-05 17:13:01 +0300

    setup.php
    Theme template setup.php file

---
+++ setup.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined( 'WPINC' ) ) exit;
+/**
+ * Theme template files setup
+ * v    0.0.1
+ */
+$template_files = array(
+	'rows-content.dat',
+	'rows-intro.dat',
+	'rows-shop.dat',
+);
